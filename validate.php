b290d87c9709c92a5cfa524ea97ab72420966a32
joheng05 <79580544+joheng05@users.noreply.github.com>
2021-02-24 21:54:48 +0100

    Add files via upload

--- validate.php
+++ validate.php
@@ -1,23 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-    <body>
-    
-        <?php
+<?php
         
             $name = $_POST['name'];
             $email = $_POST['email'];
             $msg = $_POST['msg'];
-            $nameErr = $_POST['nameErr'];
-            $emailErr = $_POST['emailErr'];
+            //$nameErr = $_POST['nameErr'];
+            //$emailErr = $_POST['emailErr'];
         
             if(isset($_POST['submit'])){
                 $to = "wisam.hammadeh@gmail.com"; // this is your Email address
                 $from = $email; // this is the sender's Email address
-                $name;
+          
                 $subject = "Form submission";
                 $subject2 = "Copy of your form submission";
                 $message = $name . " wrote the following:" . "\n\n" . $msg;
-                $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+                $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['msg'];
 
                 $headers = "From:" . $from;
                 $headers2 = "From:" . $to;
@@ -30,6 +26,3 @@
             
         
         ?>
-    
-    </body>
-</html>
